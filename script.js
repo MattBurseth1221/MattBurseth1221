63019832ce9424beba936b32f67bc32db9f7f697
Matthew Burseth <58889470+MattBurseth1221@users.noreply.github.com>
2022-08-28 13:03:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function updateAge() {
+	var age = -1
+	var currentdate = new Date();
+	var year = currentdate.getFullYear();
+	var month = currentdate.getMonth();
+	var day = currentdate.getDay();
 
+	if (month == 12) {
+		if (day > 20) {
+			age = year - 2000;
+		}
+	} else {
+		age = year - 2001;
+	}
+
+	document.getElementById("age-tag").innerHTML = age;
+	//alert("i am " + age + " years old.");
+}
