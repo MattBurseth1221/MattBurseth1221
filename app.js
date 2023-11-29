5d9012e82b8d9b4c65d999ea99eb3886f6c5b972
Matthew Burseth <mattburseth1@gmail.com>
2023-11-29 13:25:17 -0600

    Update main page layout

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // Spotify API credentials
 const CLIENT_ID = "b7a28fe4fa8e4a13846d6dd5579fd5f9";
-const REDIRECT_URI = "http://127.0.0.1:5500/index.html";
+const REDIRECT_URI = "http://localhost:5502/index.html";
 const SCOPES = ["user-read-private", "user-read-email", "user-top-read"];
 
 const WORDNIK_API_KEY = "vi2bx8wan21fjdix6j7aqiiejjhp5a01i8konq6k9b1us4rvo";
 
 const BASE_API_CALL = "https://api.spotify.com.";
+const SPOTIFY_SEARCH_CALL = "https://api.spotify.com/v1/search";
 var APIResponse;
 var accessToken = "null";
 localStorage.setItem("accessToken", "null");
@@ -28,34 +29,28 @@ function getAccessTokenFromHash() {
 
   if (localStorage.getItem("accessToken") == "null") {
     localStorage.setItem("accessToken", accessTokenOld);
+    accessToken = accessTokenOld;
     console.log("access token changed: " + localStorage.getItem("accessToken"));
   }
 }
 
-if (accessToken == "null") {
-  getAccessTokenFromHash();
-  accessToken = localStorage.getItem("accessToken");
-}
+// if (accessToken == "null") {
+//   getAccessTokenFromHash();
+//   accessToken = localStorage.getItem("accessToken");
+// }
 
 // Display the access token in a paragraph tag
 async function success() {
-  const accessTokenParagraph = document.getElementById("hasToken");
-
   if (accessToken != "null") {
-    accessTokenParagraph.textContent = "Access token received!";
-    //console.log(accessToken);
-
     APIResponse = await fetchProfile(accessToken);
-
-    setSuccessSpan();
 
     document.getElementById("profile-name-span").textContent =
       APIResponse.display_name;
     document.getElementById("profile-country-span").textContent =
       APIResponse.country;
   } else {
-    accessTokenParagraph.textContent =
-      "No Access Token found, please log in to Spotify on the Services page.";
+    // accessTokenParagraph.textContent =
+    //   "No Access Token found, please log in to Spotify on the Services page.";
   }
   //document.body.appendChild(accessTokenParagraph);
 }
@@ -169,4 +164,23 @@ function setSuccessSpan() {
   const successSpan = document.getElementById("successSpan");
 
   successSpan.textContent = "Success";
+}
+
+window.addEventListener("load", () => {
+  getAccessTokenFromHash();
+
+  if (localStorage.getItem("accessToken") != "null") {
+    success();
+  } else {
+    alert("Please sign in to Spotify on the Services page");
+  }
+});
+
+async function searchForSong() {
+  const res = await fetch(SPOTIFY_SEARCH_CALL, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    data: 
+  })
 }
