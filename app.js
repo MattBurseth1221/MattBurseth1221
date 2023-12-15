8d42f9b4b725e8c806afcf4ac914fe9d263bf090
Matthew Burseth <mattburseth1@gmail.com>
2023-12-14 19:58:14 -0600

    Added sign up page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const CLIENT_ID = "b7a28fe4fa8e4a13846d6dd5579fd5f9";
 const SCOPES = ["user-read-private", "user-read-email", "user-top-read"];
 
+const MAIN_SITE = "http://localhost:5502/index.html";
 const nodeServerHTTPS = "http://localhost:8443";
 const nodeServerHTTP = "https://192.168.0.7:8443";
 const nodeServer = nodeServerHTTPS;
@@ -301,3 +302,50 @@ async function addDBUser() {
 
   console.log(content);
 }
+
+function toggleLoginForm() {}
+
+async function signUpUser(formData) {
+  console.log("formdata: " + formData);
+
+  localStorage.setItem(
+    "formData",
+    JSON.stringify(Object.fromEntries(formData))
+  );
+
+  const signupResult = await fetch(nodeServer + `/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(Object.fromEntries(formData)),
+  }).then((response) => response.json());
+
+  var signupMessage = document.getElementById("signup-error");
+  localStorage.setItem("signup", signupResult);
+
+  if ("error" in signupResult) {
+    signupMessage.textContent = signupResult.error;
+    signupMessage.style.color = "red";
+
+    //window.location.href = MAIN_SITE;
+
+    // Back out of function, let user try another username
+    return false;
+  } else {
+    signupMessage.textContent = signupResult.success;
+    signupMessage.style.color = "green";
+
+    window.setTimeout(function () {
+      // Move to a new location or you can do something else
+      window.location.href = MAIN_SITE;
+    }, 3000);
+  }
+
+  return false;
+}
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  signUpUser(new FormData(e.target));
+});
